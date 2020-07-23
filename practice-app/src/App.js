e2fe8a27e8fe5447aed1359b4521749c681c6087
Sivazaq <sivaram.chowdary3@gmail.com>
2020-07-23 16:48:51 +0530

    routing

--- practice-app/src/App.js
+++ practice-app/src/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
+
+import Home from './home/Home';
+import About from './about/About';
+
+import {BrowserRouter as Router, Link, Switch, Route} from 'react-router-dom';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h4> Welcome to react</h4>
+      <Router>
+      <div>
+        <Link to='/'> HOME</Link>
+        <Link to='/about'> ABOUT</Link>
+      </div>
+
+      <Switch>
+        <Route exact path='/'  component={Home}  />
+        <Route exact path='/about'   component={About} />
+      </Switch>
+      </Router>
+      
     </div>
   );
 }
