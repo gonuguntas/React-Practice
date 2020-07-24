387171b1c9900069ab3004306f1a8ce10e29b799
Sivazaq <sivaram.chowdary3@gmail.com>
2020-07-24 17:06:52 +0530

    'Props'

---
+++ practice-app/src/home/FormExampleChild.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+Function child1 (){
+    return(
+        <div>
+            firstName<input type='text'></input>
+            <label>child component </label>
+
+        </div>
+    )
+}
+
+export default child1;
+
---
+++ practice-app/src/home/FormExampleParent.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import child1 from './FormExampleChild'
+
+class Parent1 extends React.Component{
+    constructor(props){
+        super(props);
+
+        this.state={
+            firstName:'',
+            lastName:''
+        }
+
+    
+    
+    
+    
+    
+    
+    }
+}
+
+export default Parent1;
